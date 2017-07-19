2d9c7b7c3659af96033a3daea6bdcd13429de03e
milosbunijevac <milos.bunijevac@gmail.com>
2017-07-19 05:15:02 -0400

    "start of chap9 lecture 124"

--- src/index.js
+++ src/index.js
@@ -2,14 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index.js';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
+
+class Goodbye extends React.Component {
+  render() {
+    return (
+      <div>
+        Goodbye
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));

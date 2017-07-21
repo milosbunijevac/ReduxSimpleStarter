0a9022d895e623c3b7b27aa0a5ac9af461c56dbc
milosbunijevac <milos.bunijevac@gmail.com>
2017-07-21 00:11:39 -0400

    "start of chapter 9 lecture 129"

---
+++ src/components/posts_new.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class PostsNew extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        NEW POSTS!
+      </div>
+    )
+  }
+}
+
+export default PostsNew;

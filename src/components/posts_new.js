f941928f10eb0eb9eb5905010c56df10e08b0328
milosbunijevac <milos.bunijevac@gmail.com>
2017-08-03 18:35:57 -0400

    "start of lecture 137 chapter 9"

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,17 +1,75 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
   constructor(props){
     super(props);
   }
 
+  renderField(field) {
+    return (
+      <div>
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
+  renderTagsField() {
+
+  }
+
   render() {
     return(
       <div>
-        NEW POSTS!
+        <form>
+          <Field
+            label="Title for Post"
+            name="title"
+            component={this.renderField}
+          />
+          <Field
+            label="Categories"
+            name="categories"
+            component={this.renderField}
+          />
+          <Field
+            label="Post Content"
+            name="content"
+            component={this.renderField}
+          />
+        </form>
       </div>
     )
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  //console.log(values) -> {title:'asdf', categories: 'asdf', content: 'asdf}
+  const errors = {};
+
+  //Validate the inputs from 'values'
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+
+  if(!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+
+  if(!values.content) {
+    errors.content = "Enter some content please";
+  }
+
+  //If errors is empty the form is fine to submit.
+  return errors;
+}
+
+export default reduxForm({
+  validate: validate,
+  form: 'PostsNewForm'
+})(PostsNew);
